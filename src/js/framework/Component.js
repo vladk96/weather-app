5d82eab86768f88bff08b77aec8ba068ed89869c
vladk96 <vladk961@gmail.com>
2019-03-12 23:04:02 +0200

    add framework: Component

---
+++ src/js/framework/Component.js
@@ -0,0 +1,90 @@
+export default class Component {
+  constructor(host, props = {}){
+    this.host = host;
+    this.props = props;
+    this.bindEvething();
+    this._render();
+  }
+
+  bindEvething() {
+  }
+
+  _render(){
+    let content = this.render();
+
+    if (!Array.isArray(content)) {
+      content = [ content ];
+    }
+
+    content.map( item => this._vDomPrototypeElementToHTMLElement(item))
+      .forEach(htmlElement => {
+        this.host.appendChild(htmlElement);
+      });
+  }
+
+  render () {
+    return 'Nothing';
+  }
+
+  _vDomPrototypeElementToHTMLElement(element) {
+    if (typeof element === 'string') {
+      let container;
+      const containsHtmlTags = /[<>&]/.test(element);
+
+      if (containsHtmlTags) {
+        container = document.createElement('div');
+        container.innerHTML = element;
+      } else {
+        container = document.createTextNode(element);
+      }
+      return container;
+    } else {
+      if (element.tag) {
+        if (typeof element.tag === 'function') {
+          const container =  document.createDocumentFragment();
+          new element.tag(container, element.props);
+          return container;
+        } else {
+          const container = document.createElement(element.tag);
+          
+          if (element.content) {
+            container.innerHTML = element.content;
+          }
+
+          ['classList', 'attributes', 'children', 'eventHandler'].forEach( item => {
+            if (element[item] && !Array.isArray(element[item])) {
+              element[item] = [ element[item] ];
+            }
+          });
+
+          if (element.classList) {
+            container.classList.add(...element.classList);
+          }
+
+          if (element.attributes) {
+            element.attributes.forEach( attributeSpec => {
+              container.setAttribute(attributeSpec.name, attributeSpec.value);
+            });
+          }
+
+          if (element.eventHandler) {
+            element.eventHandler.forEach( item => {
+              container.addEventListener(item.eventType, item.hadler);
+            });
+          }
+
+          if (element.children) {
+            element.children.forEach(el => {
+              const htmlElement = this._vDomPrototypeElementToHTMLElement(el);
+              container.appendChild(htmlElement);
+            });
+          }
+
+          return container;
+        }
+      }
+
+      return element;
+    }
+  }
+}
