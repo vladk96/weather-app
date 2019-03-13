01fa6966720a13d956c3bbcabbc2579f88880d6e
vladk96 <vladk961@gmail.com>
2019-03-13 15:53:11 +0200

    import images to object of constants

---
+++ src/js/images.js
@@ -0,0 +1,21 @@
+import brokenClouds from '../images/weather-icons/broken-clouds.svg';
+import clearSky from '../images/weather-icons/clear-sky.svg';
+import fewClouds from '../images/weather-icons/few-clouds.svg';
+import mist from '../images/weather-icons/mist.svg';
+import rain from '../images/weather-icons/rain.svg';
+import scatteredClouds from '../images/weather-icons/scattered-clouds.svg';
+import showerRain from '../images/weather-icons/shower-rain.svg';
+import snow from '../images/weather-icons/snow.svg';
+import thunderstorm from '../images/weather-icons/thunderstorm.svg';
+
+export const weatherImages = {
+  brokenClouds,
+  clearSky,
+  fewClouds,
+  mist,
+  rain,
+  scatteredClouds,
+  showerRain,
+  snow,
+  thunderstorm,
+};
