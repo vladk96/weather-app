e62c283bdc1149b33497332937a7ece2d60aa561
vladk96 <vladk961@gmail.com>
2019-03-13 20:18:08 +0200

    modified App component

--- src/js/Components/App/App.js
+++ src/js/Components/App/App.js
@@ -1,22 +1,71 @@
 import Component from '../../framework/Component';
 import { SearchBar } from '../SearchBar/';
+import { CurrentWeather } from '../CurrentWeather/';
+import { WeatherForecast } from '../WeatherForecast/';
+import { weatherImages } from '../../images';
 
 export default class App extends Component {
   constructor(host, props) {
     super(host, props);
-    this.props = props;
   }
   
   render(){
-    // return [temp1, temp2, document.createElement('div')];
     return [
       {
         tag: 'div',
         classList: ['app-content'],
         children: [
           { tag: SearchBar },
+          { tag: CurrentWeather,
+            props: {
+              cityName: 'Bangalore, IN',
+              date: '2019-03-06',
+              weatherDesc: 'Thunderstorms',
+              weatherImage: weatherImages.thunderstorm,
+              wind: 5,
+              humidity: 70,
+              plessure: 746,
+              temperature: 15,
+            },
+          },
+          { 
+            tag: WeatherForecast,
+            props: {
+              days: [
+                {
+                  day: 'Mon',
+                  temp: '15'
+                },
+                {
+                  day: 'Tue',
+                  temp: '10'
+                },
+                {
+                  day: 'Wed',
+                  temp: '1'
+                },
+                {
+                  day: 'Thu',
+                  temp: '5'
+                },
+                {
+                  day: 'Fri',
+                  temp: '16'
+                },
+                {
+                  day: 'Sat',
+                  temp: '25'
+                },
+                {
+                  day: 'Sun',
+                  temp: '19'
+                }
+              ],
+            },
+          },
         ]
       }
     ];
   }
+  
 }
