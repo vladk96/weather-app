83baec07de8dcf5d62170e8316fd54817bc288fc
vladk96 <vladk961@gmail.com>
2019-03-27 17:22:39 +0200

    fix issue with images

--- src/js/images.js
+++ src/js/images.js
@@ -1,21 +1,21 @@
-import brokenClouds from '../images/weather-icons/broken-clouds.svg';
-import clearSky from '../images/weather-icons/clear-sky.svg';
-import fewClouds from '../images/weather-icons/few-clouds.svg';
+import drizzle from '../images/weather-icons/drizzle.svg';
+import clear from '../images/weather-icons/clear.svg';
+import clouds from '../images/weather-icons/clouds.svg';
 import mist from '../images/weather-icons/mist.svg';
 import rain from '../images/weather-icons/rain.svg';
 import scatteredClouds from '../images/weather-icons/scattered-clouds.svg';
-import showerRain from '../images/weather-icons/shower-rain.svg';
+// import rain from '../images/weather-icons/rain.svg';
 import snow from '../images/weather-icons/snow.svg';
 import thunderstorm from '../images/weather-icons/thunderstorm.svg';
 
 export const weatherImages = {
-  brokenClouds,
-  clearSky,
-  fewClouds,
+  drizzle,
+  clear,
+  clouds,
   mist,
   rain,
   scatteredClouds,
-  showerRain,
+  rain,
   snow,
   thunderstorm,
 };
