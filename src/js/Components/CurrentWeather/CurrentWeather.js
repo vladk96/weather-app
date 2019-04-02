884bf1e77980d4174b8a0e19fe46b96587a3333b
vladk96 <vladk961@gmail.com>
2019-04-02 17:05:23 +0300

    add an opportunity of saving to favourite list

--- src/js/Components/CurrentWeather/CurrentWeather.js
+++ src/js/Components/CurrentWeather/CurrentWeather.js
@@ -9,7 +9,12 @@ export default class CurrentWeather extends Component {
   }
   
   init() {
-    this.updateMyself = this.updateMyself.bind(this);
+    ['addToFavoriteList', 'updateMyself']
+      .forEach(methodName => this[methodName] = this[methodName].bind(this));
+    
+    this.state = {
+      favoriteCities: JSON.parse(localStorage.getItem('favoriteCities')) || [],
+    };
   }
 
   updateMyself(subState) {
@@ -17,109 +22,137 @@ export default class CurrentWeather extends Component {
     
   }
 
+  addToFavoriteList() {
+    let favoriteArray = [...this.state.favoriteCities];
+    const formatedCity = `${this.state.currentWeather.name}, ${this.state.currentWeather.country}`;
+    const indexSearchedCity = favoriteArray.findIndex(favoriteCity => favoriteCity === formatedCity);
+
+    if (indexSearchedCity !== -1) {
+      favoriteArray.splice(indexSearchedCity, 1);
+    } else {
+      favoriteArray.unshift(formatedCity);
+    }
+
+    localStorage.setItem('favoriteCities', JSON.stringify(favoriteArray));
+
+    AppState.update('CHANGECITY', {
+      favoriteCities: favoriteArray,
+    });
+    
+  }
+
   render() {
     if (this.state.currentWeather === undefined) {
       return '';
     } else {
-    return [
-      {
-        tag: 'section',
-        classList: ['today-weather'],
-        children: [
-          {
-            tag: 'div',
-            classList: ['wrapper'],
-            children: [
-              {
-                tag: 'header',
-                children: [
-                  {
-                    tag: 'h2',
-                    classList: ['city-name'],
-                    content: `${this.state.currentWeather.name}, ${this.state.currentWeather.country}`,
-                  },
-                  {
-                    tag: 'button',
-                    classList: ['favorite-btn'],
-                    attributes: [
-                      {
-                        name: 'type',
-                        value: 'button',
+
+      const iconStarType = 
+      (this.state.favoriteCities
+        .includes(`${this.state.currentWeather.name}, ${this.state.currentWeather.country}`)) ?
+      ['icon-star'] : ['icon-star-outlined'];
+
+      return [
+        {
+          tag: 'section',
+          classList: ['today-weather'],
+          children: [
+            {
+              tag: 'div',
+              classList: ['wrapper'],
+              children: [
+                {
+                  tag: 'header',
+                  children: [
+                    {
+                      tag: 'h2',
+                      classList: ['city-name'],
+                      content: `${this.state.currentWeather.name}, ${this.state.currentWeather.country}`,
+                    },
+                    {
+                      tag: 'button',
+                      classList: ['favorite-btn'],
+                      eventHandlers: {
+                        click: this.addToFavoriteList,
                       },
-                      {
-                        name: 'title',
-                        value: 'Add to favorites'
-                      },
-                    ],
-                    children: [
-                      {
-                        tag: 'span',
-                        classList: ['icon-star-outlined'], // props
-                      },
-                    ],
-                  },
-                ],
-              },
-              {
-                tag: 'p',
-                content: `${this.state.currentWeather.desc}`, // props
-                classList: ['weather-desc'],
-              },
-              {
-                tag: 'p',
-                content: `${this.state.currentWeather.date}`, // props
-                classList: ['date'],
-              },
-              {
-                tag: 'div',
-                classList: ['weather-image'],
-                children: [
-                  {
-                    tag: 'img',
-                    attributes: [
-                      {
-                        name: 'src',
-                        value: this.state.weatherImage, // props
-                      },
-                      {
-                        name: 'alt',
-                        value: 'weather-icon'
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            tag: 'div',
-            classList: 'wrapper',
-            children: [
-              {
-                tag: 'p',
-                content: `Wind speed: ${this.state.currentWeather.wind} ` + (this.state.unit === 'imperial' ? `mph` : `m/s`), // props
-                classList: ['wind-speed'],
-              },
-              {
-                tag: 'p',
-                content: `Humidity ${this.state.currentWeather.humidity} %`, // props
-                classList: ['humidity'],
-              },
-              {
-                tag: 'p',
-                content: `Pressure ${this.state.currentWeather.pressure} mmhg`, // props
-                classList: ['plessure'],
-              },
-              {
-                tag: 'p',
-                content: `${this.state.currentWeather.temp}&deg`, // props
-                classList: ['temperature'],
-              },
-            ],
-          },
-        ],
-      },
-    ];
+                      attributes: [
+                        {
+                          name: 'type',
+                          value: 'button',
+                        },
+                        {
+                          name: 'title',
+                          value: 'Add to favorites'
+                        },
+                      ],
+                      children: [
+                        {
+                          tag: 'span',
+                          classList: iconStarType,
+                        },
+                      ],
+                    },
+                  ],
+                },
+                {
+                  tag: 'p',
+                  content: `${this.state.currentWeather.desc}`, // props
+                  classList: ['weather-desc'],
+                },
+                {
+                  tag: 'p',
+                  content: `${this.state.currentWeather.date}`, // props
+                  classList: ['date'],
+                },
+                {
+                  tag: 'div',
+                  classList: ['weather-image'],
+                  children: [
+                    {
+                      tag: 'img',
+                      attributes: [
+                        {
+                          name: 'src',
+                          value: this.state.weatherImage, // props
+                        },
+                        {
+                          name: 'alt',
+                          value: 'weather-icon'
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              tag: 'div',
+              classList: 'wrapper',
+              children: [
+                {
+                  tag: 'p',
+                  content: `Wind speed: ${this.state.currentWeather.wind} ` + (this.state.unit === 'imperial' ? `mph` : `m/s`), // props
+                  classList: ['wind-speed'],
+                },
+                {
+                  tag: 'p',
+                  content: `Humidity ${this.state.currentWeather.humidity} %`, // props
+                  classList: ['humidity'],
+                },
+                {
+                  tag: 'p',
+                  content: `Pressure ${this.state.currentWeather.pressure} mmhg`, // props
+                  classList: ['plessure'],
+                },
+                {
+                  tag: 'p',
+                  content: `${this.state.currentWeather.temp}&deg`, // props
+                  classList: ['temperature'],
+                },
+              ],
+            },
+          ],
+        },
+      ];
     }
     
   }
--- src/js/Components/FavouriteLocations/FavouriteLocations.js
+++ src/js/Components/FavouriteLocations/FavouriteLocations.js
@@ -1,55 +1,86 @@
 import Component from '../../framework/Component';
+import AppState from '../../Services/AppState';
 
 export default class FavouriteLocations extends Component {
   constructor(host, props) {
     super(host, props);
+
+    AppState.watch('CHANGECITY', this.updateMyself);
+  }
+
+  init() {
+    ['updateMyself']
+      .forEach(methodName => this[methodName] = this[methodName].bind(this));
+    
+    this.state = {
+      favoriteCities: JSON.parse(localStorage.getItem('favoriteCities')) || [],
+    };
+  }
+
+  updateMyself(subState) {
+    this.updateState(subState);
+    
+  }
+
+  cleanAllFavorites() {
+    localStorage.removeItem('favoriteCities');
+    AppState.update('CHANGECITY', {
+      favoriteCities: [],
+    });
   }
   
   render() {
-    return [
-      {
-        tag: 'section',
-        classList: ['list', 'favorite-list'],
-        children: [
-          {
-            tag: 'header',
-            classList: ['list-head'],
-            children: [
-              {
-                tag: 'h2',
-                content: 'Favorite',
-                classList: ['list-title'],
-              },
-              {
-                tag: 'button',
-                classList: ['list-button'],
-                attributes: [
-                  {
-                    name: 'type',
-                    value: 'button',
+    if(this.state.favoriteCities.length !== 0) {
+      return [
+        {
+          tag: 'section',
+          classList: ['list', 'favorite-list'],
+          children: [
+            {
+              tag: 'header',
+              classList: ['list-head'],
+              children: [
+                {
+                  tag: 'h2',
+                  content: 'Favorite',
+                  classList: ['list-title'],
+                },
+                {
+                  tag: 'button',
+                  classList: ['list-button'],
+                  eventHandlers: {
+                    click: this.cleanAllFavorites,
                   },
-                ],
-                children: [
-                  {
-                    tag: 'span',
-                    classList: ['icon-bin'],
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            tag: 'ul',
-            children: this.props.favoriteCities.map(city => {
-              return {
-                tag: 'li',
-                content: `${city.location}`,
-              };
-            }), //props
-          },
-        ],
-      },
-    ];
+                  attributes: [
+                    {
+                      name: 'type',
+                      value: 'button',
+                    },
+                  ],
+                  children: [
+                    {
+                      tag: 'span',
+                      classList: ['icon-bin'],
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              tag: 'ul',
+              children: this.state.favoriteCities.map(city => {
+                return {
+                  tag: 'li',
+                  content: `${city}`,
+                };
+              }), //props
+            },
+          ],
+        },
+      ];
+    } else {
+      return "";
+    }
   }
 
 }
